3b5d49ba6d78675374faba4d93e633011f5a2265
Pavel Ponec <pponec@gmail.com>
2022-02-21 18:36:03 +0100

    Simple form element example

---
+++ src/main/java/net/ponec/demo/servlet/FormServlet.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020-2022 Pavel Ponec, https://github.com/pponec/demo-ajax
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ponec.demo.servlet;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.ujorm.tools.web.Element;
+import org.ujorm.tools.web.HtmlElement;
+import org.ujorm.tools.web.ao.HttpParameter;
+import static net.ponec.demo.servlet.FormServlet.Attrib.NOTE;
+
+/**
+ * A live example of the HtmlElement inside a Servlet using a ujo-web library.
+ *
+ * @author Pavel Ponec
+ * @see <a href=https://github.com/pponec/demo-ajax">github.com/pponec/demo-ajax</a>
+ */
+@WebServlet("/form-servlet")
+public class FormServlet extends HttpServlet {
+    /** Logger */
+    private static final Logger LOGGER = Logger.getLogger(FormServlet.class.getName());
+
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(
+            final HttpServletRequest request,
+            final HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try ( HtmlElement html = HtmlElement.niceOf(response, "/css/regexp.css")) {
+            try (Element body = html.addBody()) {
+                body.addHeading("Hello, World!");
+                try (Element form = body.addForm("form-inline")) {
+                    form.addLabel("control-label").addText("Note:");
+                    form.addInput("form-control", "col-lg-2")
+                            .setNameValue(NOTE, NOTE.of(request));
+                    form.addSubmitButton("btn", "btn-primary")
+                            .addText("Submit");
+                }
+            }
+        }
+    }
+
+    /** Servlet attributes */
+    enum Attrib implements HttpParameter {
+        NOTE;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+}
